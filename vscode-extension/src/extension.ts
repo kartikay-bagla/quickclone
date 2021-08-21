12191210b8a7ca549a7bd8468c03c3ec5a31675c
kartikay-bagla <kartikaybagla@gmail.com>
2021-08-21 21:07:20 +0530

    Added user settings in vscode extension

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -2,63 +2,66 @@ import * as vscode from 'vscode';
 const path = require('path');
 import { exec } from 'child_process';
 
-// this method is called when your extension is activated
-export function activate(context: vscode.ExtensionContext) {
+export const getParams = (uri: vscode.Uri): { [string: string]: string } => {
+	// TODO: Works for only a specific endpoint instead of all of em
 
-	const getParams = (uri: vscode.Uri): { [string: string]: string } => {
-		// TODO: Works for only a specific endpoint instead of all of em
+	const params: { [string: string]: string } = {};
+	uri.query
+		.split('&')
+		.forEach(param => {
+			const [key, value]: Array<string> = param.split('=');
 
-		const params: { [string: string]: string } = {};
-		uri.query
-			.split('&')
-			.forEach(param => {
-				const [key, value]: Array<string> = param.split('=');
-				params[key] = value;
-			});
-
-		return params;
-	};
-
-
-	const getRepoName = (remoteUrl: string): string | null => {
-		const split: Array<string> = remoteUrl.split('/');
-		if (split.length !== 2) {
-			return null;
-		}
-		return split[1];
-	};
-
-
-	const getCloneCommand = (remoteUrl: string, remoteType: string): string | null => {
-		switch (remoteType) {
-			case "HTTPS":
-				return "git clone https://github.com/" + remoteUrl + ".git";
-			case "SSH":
-				return "git clone git@github.com:" + remoteUrl + ".git";
-			case "GitHub CLI":
-				return "gh repo clone " + remoteUrl;
-			default:
-				return null;
-		}
-	};
-
-
-	const cloneIntoFolder = (cloneCommand: string, folderPath: string, repoName: string) => {
-		exec(cloneCommand, {
-			cwd: path.resolve(folderPath, ''),
-		}, (err, stdout, stderr) => {
-			if (err) {
-				vscode.window.showErrorMessage(err.toString());
-				return;
+			if (!value) {
+				throw new Error(`Invalid query parameter: ${param}`);
 			}
 
-			const repoPath = path.resolve(folderPath, repoName);
-			openFolderInVscode(repoPath);
+			params[key] = value;
 		});
-	};
+
+	return params;
+};
 
 
-	const openFolderInVscode = (folderPath: string) => {
+export const getRepoName = (remoteUrl: string): string | null => {
+	const split: Array<string> = remoteUrl.split('/');
+	if (split.length !== 2) {
+		return null;
+	}
+	return split[1];
+};
+
+
+export const getCloneCommand = (remoteUrl: string, remoteType: string): string | null => {
+	switch (remoteType) {
+		case "HTTPS":
+			return "git clone https://github.com/" + remoteUrl + ".git";
+		case "SSH":
+			return "git clone git@github.com:" + remoteUrl + ".git";
+		case "GitHub CLI":
+			return "gh repo clone " + remoteUrl;
+		default:
+			return null;
+	}
+};
+
+
+export const cloneIntoFolder = (cloneCommand: string, folderPath: string, repoName: string, openFolder: boolean) => {
+	exec(cloneCommand, {
+		cwd: path.resolve(folderPath, ''),
+	}, (err, stdout, stderr) => {
+		if (err) {
+			vscode.window.showErrorMessage(err.toString());
+			return;
+		}
+
+		const repoPath = path.resolve(folderPath, repoName);
+		openFolderInVscode(repoPath, openFolder);
+	});
+};
+
+
+export const openFolderInVscode = (folderPath: string, openFolder: boolean) => {
+	if (!openFolder) {
 		vscode.window.showInformationMessage(
 			'Successfully cloned the repository.' + "\n" +
 			'Do you want to open the repository in vscode?',
@@ -69,12 +72,19 @@ export function activate(context: vscode.ExtensionContext) {
 				vscode.commands.executeCommand('vscode.openFolder', uri);
 			}
 		});
-	};
+	} else {
+		let uri = vscode.Uri.file(folderPath);
+		vscode.commands.executeCommand('vscode.openFolder', uri);
+	}
+};
 
-
+export function activate(context: vscode.ExtensionContext) {
 
 	vscode.window.registerUriHandler({
 		handleUri: (uri: vscode.Uri) => {
+
+			const vscodeConfig = vscode.workspace.getConfiguration("oneclickgitclone");
+
 			// TODO: Sanitize remoteUrl before passing it down
 			const { remoteUrl, remoteType } = getParams(uri);
 			if (!remoteUrl) {
@@ -97,12 +107,17 @@ export function activate(context: vscode.ExtensionContext) {
 				return;
 			}
 
+			const defaultFolder: string | undefined = vscodeConfig.get("defaultFolder");
+			const defaultUri = defaultFolder !== undefined && defaultFolder !== ""
+				? vscode.Uri.file(defaultFolder) : undefined;
+
 			const dialogOptions = {
 				canSelectFiles: false,
 				canSelectFolders: true,
 				canSelectMany: false,
 				openLabel: 'Select a folder',
-				title: 'Select folder to clone repository into'
+				title: 'Select folder to clone repository into',
+				defaultUri: defaultUri
 			};
 			vscode.window.showOpenDialog(dialogOptions)
 				.then(folders => {
@@ -110,7 +125,8 @@ export function activate(context: vscode.ExtensionContext) {
 						vscode.window.showInformationMessage(`Cloning into ${repoName}...`);
 
 						const folderPath = folders[0].fsPath;
-						cloneIntoFolder(cloneCommand, folderPath, repoName);
+						const openFolder: boolean = vscodeConfig.get("alwaysOpen") ?? false;
+						cloneIntoFolder(cloneCommand, folderPath, repoName, openFolder);
 					}
 					else {
 						vscode.window.showInformationMessage('Operation cancelled.');
